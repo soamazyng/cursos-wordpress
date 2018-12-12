3dd9e6a8d572b80b25c9606db03433772d672fe1
Jaqueline Benedicto <soamazing@gmail.com>
2018-12-12 18:57:55 -0200

    +

---
+++ udemy-ultimate_wordpress_plugin_course/wp-content/plugins/snappy-list-builder-jay/snappy-list-builder-jay.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+  Plugin Name: Snappy List Builder Jay
+  Plugin URI: http://wordpressplugincourse.com/plugins/snappy-list-builder
+  Description: The ultimate list building....
+  version: 1.0
+  Author: Jay @ Code
+  Author URI: http://www.jay.com.br
+  License: GPLV2  
+*/
+
+
+/* !0. TABLE OF CONTENTS */
+
+/*
+	
+	1. HOOKS
+	
+	2. SHORTCODES
+		
+	3. FILTERS
+		
+	4. EXTERNAL SCRIPTS
+		
+	5. ACTIONS
+		
+	6. HELPERS
+		
+	7. CUSTOM POST TYPES
+	
+	8. ADMIN PAGES
+	
+	9. SETTINGS
+	
+	10. MISCELLANEOUS 
+
+*/
+
+
+
+
+/* !1. HOOKS */
+
+
+
+
+/* !2. SHORTCODES */
+
+
+
+
+/* !3. FILTERS */
+
+
+
+
+/* !4. EXTERNAL SCRIPTS */
+
+
+
+
+/* !5. ACTIONS */
+
+
+
+
+/* !6. HELPERS */
+
+
+
+
+/* !7. CUSTOM POST TYPES */
+
+
+
+
+/* !8. ADMIN PAGES */
+
+
+
+
+/* !9. SETTINGS */
+
+
+
+
+/* !10. MISCELLANEOUS */
+
+
+
